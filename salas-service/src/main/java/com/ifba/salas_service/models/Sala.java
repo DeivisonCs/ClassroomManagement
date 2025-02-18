17c05dc1ae3ab0013c8397436b0523c94af8eba4
Pedro Henrique Barreto Velloso <pedrohenriquevelloso18@gmail.com>
2025-02-18 03:17:13 -0300

    Adicionando CRUD básicão de salas

---
+++ salas-service/src/main/java/com/ifba/salas_service/models/Sala.java
@@ -0,0 +1,23 @@
+package main.java.com.ifba.salas_service.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "salas")
+public class Sala {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String codigo;
+
+    @Column(nullable = false)
+    private String nome;
+}
