9c8344da4addee03d57f2db7bfbbe2bc8be8b75c
Deivison Santos <deivisonddj3@gmail.com>
2025-02-18 16:12:42 -0300

    addind link to pages

--- ClassManagement/src/pages/private/HomePage/index.tsx
+++ ClassManagement/src/pages/private/HomePage/index.tsx
@@ -2,15 +2,32 @@ import React from "react";
 import HeaderComponent from "../../../components/HeaderComponent";
 
 import "./styles.css";
+import PageLinkComponent from "../../../components/PageOption/PageLinkComponent";
+
+interface PageOptionProps {
+    label: string
+    icon: string
+    linkTo: string
+}
 
 const HomePage:React.FC = () => {
-
+    const pageOption:PageOptionProps[] = [
+        {label: 'Disciplinas', icon:'book', linkTo:'/'},
+        {label: 'Salas', icon:'building-columns', linkTo:'/'},
+        {label: 'Usuários', icon:'user', linkTo:'/'}
+    ]
     //TODO 
     // Verificar se o usuário está realmente logado, para bloquear acesso pela url
 
     return(
         <section id="home-section">
             <HeaderComponent/>
+
+            <div id="page-options">
+                {pageOption.map((option, idx) => {
+                    return <PageLinkComponent key={idx} label={option.label} icon={option.icon} linkTo={option.linkTo}/>
+                })}
+            </div>
         </section>
     );
 
