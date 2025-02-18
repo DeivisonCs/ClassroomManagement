0d09a6f43cba0f8cea90e42da04d0dd9d418c96b
Deivison Santos <138685992+DeivisonCs@users.noreply.github.com>
2025-02-18 19:12:00 -0300

    Page Component

--- ClassManagement/src/pages/private/HomePage/index.tsx
+++ ClassManagement/src/pages/private/HomePage/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import HeaderComponent from "../../../components/HeaderComponent";
 
 import "./styles.css";
 import PageLinkComponent from "../../../components/PageOption/PageLinkComponent";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface PageOptionProps {
     label: string
@@ -10,21 +11,63 @@ interface PageOptionProps {
     linkTo: string
 }
 
-const HomePage:React.FC = () => {
-    const pageOption:PageOptionProps[] = [
+enum PageIdentifier {
+    HOME = 'home',
+    USERS = 'users',
+    CLASSROOM = 'classroom'
+}
+
+const pageMenuOptions: Record<PageIdentifier, PageOptionProps[]> = {
+    [PageIdentifier.HOME] : [
         {label: 'Disciplinas', icon:'book', linkTo:'/home'},
         {label: 'Salas', icon:'building-columns', linkTo:'/home'},
         {label: 'Usuários', icon:'user', linkTo:'/home'}
-    ]
-    //TODO 
-    // Verificar se o usuário está realmente logado, para bloquear acesso pela url
+    ],
+    [PageIdentifier.USERS] : [
+        {label: 'Salas', icon:'building-columns', linkTo:'/home'},
+        {label: 'Usuários', icon:'user', linkTo:'/home'}
+    ],
+    [PageIdentifier.CLASSROOM] : [],
+}
+
+const HomePage:React.FC = () => {
+    const navigate = useNavigate();
+    let {page} = useParams();
+    const [pageOptions, setPageOptions] = useState<PageOptionProps[]>([]);
+    const [pageTitle, setPageTitle] = useState<string>('');
+
+    //TODO - Verificar se o usuário está realmente logado, para bloquear acesso pela url
+
+    useLayoutEffect(() => {
+        validUrl();
+        capitalizePageTitle();
+    }, []);
+
+    const validUrl = () => {
+        if(!Object.values(PageIdentifier).includes(page as PageIdentifier)) {
+            console.log("Página não encontrada")
+            navigate("/");
+        }
+        setPageOptions(pageMenuOptions[page as PageIdentifier || []])
+    }
+
+    function capitalizePageTitle() {
+        let str = page!.toString();
+
+        str.split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+            .join(' ');
+
+        setPageTitle(str);
+    }
 
     return(
         <section id="home-section">
             <HeaderComponent/>
+            <h1>{pageTitle}</h1>
 
             <div id="page-options">
-                {pageOption.map((option, idx) => {
+                {pageOptions.map((option, idx) => {
                     return <PageLinkComponent key={idx} label={option.label} icon={option.icon} linkTo={option.linkTo}/>
                 })}
             </div>
