4f0d897eed46933d1886f0e376d15f75d2ec62db
jvsn <jvsn.10.jv@gmail.com>
2025-03-02 01:08:27 -0300

    add OccupationType enum with JSON serialization methods

--- backend/ms-user/src/main/java/com/ifba/ms_user/models/enums/OccupationType.java
+++ backend/ms-user/src/main/java/com/ifba/ms_user/models/enums/OccupationType.java
@@ -1,7 +1,19 @@
 package com.ifba.ms_user.models.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OccupationType {
-	STUDENT,
-	TEACHER,
-	ADMIN
+	ADMINISTRADOR,
+	PROFESSOR;
+	
+	@JsonCreator
+    public static OccupationType fromString(String key) {
+        return key == null ? null : OccupationType.valueOf(key.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
