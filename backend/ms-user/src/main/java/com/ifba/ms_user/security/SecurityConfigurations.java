2649edc5d1117ec2dbdbe7ef58265136e858384a
jvsn <jvsn.10.jv@gmail.com>
2025-03-02 01:10:52 -0300

    add axios dependency and update security configurations for login and CORS; refactor account creation to use default password generation

--- backend/ms-user/src/main/java/com/ifba/ms_user/security/SecurityConfigurations.java
+++ backend/ms-user/src/main/java/com/ifba/ms_user/security/SecurityConfigurations.java
@@ -30,7 +30,8 @@ public class SecurityConfigurations {
 		return http.csrf(csrf -> csrf.disable())
 	            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 	            .authorizeHttpRequests(req -> {
-	                req.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
+	                req.requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+	                req.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 	                req.anyRequest().authenticated();
 	            })
 	            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
