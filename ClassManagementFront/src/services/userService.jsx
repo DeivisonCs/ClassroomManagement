b0b939c1b00d16022743df6586f5f2e547c0b10f
jvsn <jvsn.10.jv@gmail.com>
2025-03-04 17:49:27 -0300

    feat: implement user creation, listing, and deletion endpoints in userService

--- ClassManagementFront/src/services/userService.jsx
+++ ClassManagementFront/src/services/userService.jsx
@@ -4,13 +4,18 @@ const BASE_URL = 'http://localhost:8080';
 const BASE_ACCOUNT_ENDPOINT = '/account';
 
 const LOGIN_ENDPOINT = '/auth/login';
-const REGISTER_ENDPOINT = BASE_ACCOUNT_ENDPOINT+'/register';
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+const getAuthHeader = () => ({
+  headers: {
+    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+  }
 });
 
 export const authUser = async (user, password) => {
@@ -21,26 +26,54 @@ export const authUser = async (user, password) => {
     });
 
     return response.data;
-
   } catch (error) {
     throw error;
   }
 };
 
-export const createUser = async (occupation, name, email, cpf, registration) => {
+export const createUser = async (occupationId, name, email, cpf, registration) => {
   try {
-    const response = await api.post(REGISTER_ENDPOINT, {
-        occupation: occupation,
+    const response = await api.post(BASE_ACCOUNT_ENDPOINT, {
+        occupationId: occupationId,
         name: name,
         email: email,
         cpf: cpf,
-        humanReadableId: registration
+        registration: registration
     }, 
-    {
-      headers: {
-          'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-      }
-    });
+    getAuthHeader());
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const listOccupations = async () => {
+  try {
+    const response = await api.get(BASE_ACCOUNT_ENDPOINT + '/occupations',
+    getAuthHeader());
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const listUsers = async () => {
+  try {
+    const response = await api.get(BASE_ACCOUNT_ENDPOINT,
+    getAuthHeader());
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const deleteUser = async (userId) => {
+  try {
+    const response = await api.delete(BASE_ACCOUNT_ENDPOINT + '/' + userId,
+    getAuthHeader());
 
     return response.data;
   } catch (error) {
