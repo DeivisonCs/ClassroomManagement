a7ca8136bbb1d1e05fe32a5874d1875681894b13
jvsn <jvsn.10.jv@gmail.com>
2025-03-06 02:32:06 -0300

    Add user service with authentication and user management functions

--- ClassManagementFront/src/services/userService.js
+++ ClassManagementFront/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8082/ms-users';
 const BASE_ACCOUNT_ENDPOINT = '/account';
 
 const LOGIN_ENDPOINT = '/auth/login';
