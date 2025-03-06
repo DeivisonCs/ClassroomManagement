d3beddcf1514d051d2fdf13d225411875923eb63
Pedro Henrique Barreto Velloso <pedrohenriquevelloso18@gmail.com>
2025-03-05 22:49:49 -0300

    Ajusta aulas no backend para funcionar corretamente com o front

--- Server/salas-service/src/main/java/com/ifba/salas_service/exceptions/BusinessException.java
+++ Server/salas-service/src/main/java/com/ifba/salas_service/exceptions/BusinessException.java
@@ -1,5 +1,9 @@
 package com.ifba.salas_service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT) 
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
--- Server/salas-service/src/main/java/com/ifba/salas_service/repositories/AulaRepository.java
+++ Server/salas-service/src/main/java/com/ifba/salas_service/repositories/AulaRepository.java
@@ -3,7 +3,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ifba.salas_service.models.Aula;
+import com.ifba.salas_service.models.Disciplina;
+import com.ifba.salas_service.models.Turma;
 
 @Repository
 public interface AulaRepository extends JpaRepository<Aula, Long> {
+    boolean existsByDisciplinaAndTurma(Disciplina disciplina, Turma turma);
+    
 }
